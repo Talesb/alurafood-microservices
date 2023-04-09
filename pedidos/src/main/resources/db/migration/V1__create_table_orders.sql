619b179654f135244d166c55315e7e13dcf41455
talesb <tales.batista@temasistemas.com.br>
2023-04-09 19:29:08 -0300

    Translating order folder

--- pedidos/src/main/resources/db/migration/V1__create_table_orders.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE  orderrequest(
-  id bigint(20) NOT NULL AUTO_INCREMENT,
-  date_time_order datetime NOT NULL,
-  status varchar(255) NOT NULL,
-  PRIMARY KEY (id)
-)
--- pedidos/src/main/resources/db/migration/V2__create_table_order_items.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE order_item (
-  id bigint(20) NOT NULL AUTO_INCREMENT,
-  description varchar(255) DEFAULT NULL,
-  quantity int(11) NOT NULL,
-  orderrequest_id bigint(20) NOT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (orderrequest_id) REFERENCES orderrequest(id)
-)
